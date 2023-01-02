6d9c5c3f881da4db9c3442698b35d4c5d0dc3698
xyzuan <xyzuannihboss@gmail.com>
2023-01-02 21:52:36 +0700

    xyLang: Beautify engine with ANSI color

--- src/main/java/id/xyzprjkt/xylang/xyLanguage.java
+++ src/main/java/id/xyzprjkt/xylang/xyLanguage.java
@@ -8,18 +8,32 @@ import lombok.SneakyThrows;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
 public class xyLanguage {
+
+    /**
+     * ANSI Color Term
+     */
+    public static final String RESET = "\033[0m";
+    public static final String GREEN_BOLD = "\033[1;32m";
+    public static final String YELLOW_BOLD = "\033[1;33m";
+    public static final String YELLOW_UNDERLINED = "\033[4;93m";
+    public static final String WHITE_BOLD = "\033[1;97m";
+
     @SneakyThrows
     public void execute() {
 
         boolean exit = false;
         Scanner userInput = new Scanner(System.in);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
 
         do {
-            System.out.print("xyLang>> ");
+            LocalDateTime now = LocalDateTime.now();
+            System.out.printf(GREEN_BOLD + "[%s]" + WHITE_BOLD + " xyLang>> ", dtf.format(now));
             LexicalParser lexicalParser = new LexicalParser(userInput.nextLine());
             List<Token> tokens = lexicalParser.parse();
 
@@ -61,7 +75,7 @@ public class xyLanguage {
 
         xyLanguage main = new xyLanguage();
         if (args.length == 0) {
-            System.err.println("Realtime mode are under development");
+            System.out.println(YELLOW_UNDERLINED + "Realtime mode are under development" + RESET);
             main.execute();
         } else {
             int index = args[0].lastIndexOf('.');
