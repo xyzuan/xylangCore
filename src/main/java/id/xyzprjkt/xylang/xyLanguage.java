aa9dea63d818da76fb9bec3ced8b6d51cc21f836
xyzuan <xyzuannihboss@gmail.com>
2023-01-02 18:58:22 +0700

    xyLang: Introduce Realtime Mode [BETA]

--- src/main/java/id/xyzprjkt/xylang/xyLanguage.java
+++ src/main/java/id/xyzprjkt/xylang/xyLanguage.java
@@ -6,15 +6,39 @@ import id.xyzprjkt.xylang.statement.CompositeStatement;
 import id.xyzprjkt.xylang.token.Token;
 import lombok.SneakyThrows;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class xyLanguage {
     @SneakyThrows
-    public void execute(Path path) {
-        String source = Files.readString(path);
-        LexicalParser lexicalParser = new LexicalParser(source);
+    public void execute() {
+
+        boolean exit = false;
+        Scanner userInput = new Scanner(System.in);
+
+        do {
+            System.out.print("xyLang>> ");
+            LexicalParser lexicalParser = new LexicalParser(userInput.nextLine());
+            List<Token> tokens = lexicalParser.parse();
+
+            DefinitionContext.pushScope(DefinitionContext.newScope());
+            MemoryContext.pushScope(MemoryContext.newScope());
+            try {
+                CompositeStatement statement = new CompositeStatement();
+                StatementParser.parse(tokens, statement);
+                statement.execute();
+            } finally {
+
+            }
+        } while (!exit);
+    }
+
+    public void execute(Path params) {
+        String sources = params.toString();
+        LexicalParser lexicalParser = new LexicalParser(sources);
         List<Token> tokens = lexicalParser.parse();
 
         DefinitionContext.pushScope(DefinitionContext.newScope());
@@ -30,10 +54,18 @@ public class xyLanguage {
     }
 
     public static void main(String[] args) {
+
+        System.out.println("===========================");
+        System.out.println("xyLang rev1.0-dev");
+        System.out.println("Developed by xyzuan");
+        System.out.println("===========================");
+
         xyLanguage main = new xyLanguage();
-
-        System.out.println("xylang rev.1.0.beta\n");
-
-        main.execute(Path.of(args[0]));
+        if (Arrays.stream(args).allMatch(Objects::nonNull)) {
+            System.err.println("Realtime mode are under development");
+            main.execute();
+        } else {
+            main.execute(Path.of(args[0]));
+        }
     }
 }
